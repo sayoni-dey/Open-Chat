b31081ee606643d38c691999fcd6a21ce03f9b2a
sayoni-dey <sayonidey.official@gmail.com>
2026-07-09 21:22:56 +0530

    Rebuilding from scratch

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from  "mongoose";
-import chatRoutes from "./routes/chat.js";
 
 const app = express();
 const PORT = 8080;
-// const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(cors());
-app.use ("/api", chatRoutes);
 
 const connectDb = async() => {
     try {
@@ -25,15 +22,3 @@ app.listen(PORT, () => {
     console.log(`app is listening on port ${PORT}`);
     connectDb();
 });
-
-
-// app.post ("/test", async (req, res) => {
-//     const {prompt} = req.body;
-//     try {
-//         const reply = await hybridChat(prompt);
-//         res.json({reply});
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({error: "Something went wrong"});
-//     }
-// })
